5635633e8b2be9e0a5d5847f9d83313385dbccdb
GabrieleGrossi <grossigabriele98@gmail.com>
2023-05-16 14:14:30 +0200

    create a program on console

---
+++ myscript.js
@@ -0,0 +1,12 @@
+// Scrivere un programma che stampi in console i numeri da 1 a 100
+
+// Per tutti i multipli di 3, deve scrivere FIZZ
+
+// Per tutti i multipli di 5, deve scrivere BUZZ
+
+// Per tutti i multipli di 3 e 5, deve scrivere FIZZBUZZ
+
+for (let i = 1; i <= 100 ; i++){
+    console.log(i)
+
+}
